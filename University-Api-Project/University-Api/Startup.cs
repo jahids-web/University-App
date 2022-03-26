26943c3cd94eb476d35a15d7351f88252a3deedc
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-26 23:54:01 +0600

    Project Setup

--- University-Api-Project/University-Api/Startup.cs
+++ University-Api-Project/University-Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,38 @@ namespace University_Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            SetupSwagger(services);
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+            });
         }
+
+        private void SetupSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "University-Api",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+            });
+        }
+
+       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,7 +67,14 @@ namespace University_Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            //
+            app.UseSwagger();
 
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
+            });
+            //
             app.UseHttpsRedirection();
 
             app.UseRouting();
