b1d66f50895653b116419f801eef80b956893fe7
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-27 17:00:58 +0600

    Add DLLDependency.cs

---
+++ University-Api-Project/DLL/DLLDependency.cs
@@ -0,0 +1,16 @@
+﻿using DLL.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DLL
+{
+    public class DLLDependency
+    {
+        public static void ALLDependency(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("ConnectDatabase")));
+        }
+    }
+}
