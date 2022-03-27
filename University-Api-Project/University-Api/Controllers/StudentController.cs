2960f254d0ed58a51de062d74f11af8c844c9305
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-27 23:36:58 +0600

    Add Student Api

--- University-Api-Project/University-Api/Controllers/StudentController.cs
+++ University-Api-Project/University-Api/Controllers/StudentController.cs
@@ -1,9 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DLL.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using University_Api.Model;
 
 namespace University_Api.Controllers
 {
     public class StudentController : MainApiController
     {
+        private readonly IStudentRepository _studentReposatory;
+
+        public StudentController(IStudentRepository studentReposatory)
+        {
+            _studentReposatory = studentReposatory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(Student student)
+        {
+            return Ok(await _studentReposatory.InsertAsync(student));
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _studentReposatory.GetAllAsync());
+        }
+
+        [HttpGet("{email}")]
+        public async Task<IActionResult> GetA(string email)
+        {
+            return Ok(await _studentReposatory.GatAAsync(email));
+        }
+
+        [HttpPut("{email}")]
+        public async Task<IActionResult> Update(string email, Student student)
+        {
+            return Ok(await _studentReposatory.UpdateAsync(email, student));
+        }
+
+        [HttpDelete("{email}")]
+        public async Task<IActionResult> Delete(string email)
+        {
+            return Ok(await _studentReposatory.DeleteAsync(email));
+        }
     }
 }
