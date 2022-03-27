9ec6bc213791d8b8717043447e3ba87d289b3e46
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-27 16:08:18 +0600

    Add Dll Project

---
+++ University-Api-Project/DLL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //public DbSet<Department> Departments { get; set; }
+        //public DbSet<Student> Students { get; set; }
+
+
+    }
+}
