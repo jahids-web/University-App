565da01e3d3107b1a58f9974658d9314cf114d72
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-28 19:45:42 +0600

    Add Some Code.

--- University-Api-Project/BLL/Services/IDepartmentService.cs
+++ University-Api-Project/BLL/Services/IDepartmentService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using University_Api.Model;
+using Utility.Exceptions;
 
 namespace BLL.Services
 {
@@ -43,15 +44,54 @@ namespace BLL.Services
 
         public async Task<Department> GetAAsync(string code)
         {
-            return await _departmentRepository.GetAAsync(code);
+            var department = await _departmentRepository.GetAAsync(code);
+            if(department == null)
+            {
+                throw new ApplicationValidationExcetion("Department Not Found");
+            }
+            return department;
         }
-        public async Task<Department> UpdateAsync(string code, Department department)
+        public async Task<Department> UpdateAsync(string code, Department adepartment)
         {
-            return await _departmentRepository.UpdateAsync(code,department);
+            var department = await _departmentRepository.GetAAsync(code);
+            if (department == null)
+            {
+                throw new ApplicationValidationExcetion("Department Not Found");
+            }
+            if (!string.IsNullOrWhiteSpace(adepartment.Code))
+            {
+                var existsAlreadyCode = await _departmentRepository.FindByCode(adepartment.Code);
+                if (existsAlreadyCode != null)
+                {
+                    throw new ApplicationValidationExcetion("Your Updated Code Alredy Present System");
+                }
+                department.Code = adepartment.Code;
+            }
+            if (!string.IsNullOrWhiteSpace(adepartment.Name))
+            {
+                var existsAlreadyCode = await _departmentRepository.FindByName(adepartment.Name);
+                if (existsAlreadyCode != null)
+                {
+                    throw new ApplicationValidationExcetion("Your Updated Name Alredy Present System");
+                }
+                department.Name = adepartment.Name;
+            }
+            if(await _departmentRepository.UpdateAsync(department))
+            {
+                return department;
+            }
+            throw new ApplicationValidationExcetion("Department Not Found");
         }
         public async Task<Department> DeleteAsync(string code)
         {
-            return await _departmentRepository.DeleteAsync(code);
+            var department = await _departmentRepository.GetAAsync(code);
+            if(department == null){
+                throw new ApplicationValidationExcetion("Department Not Found");
+            }
+            if( await _departmentRepository.DeleteAsync(department)){
+                return department;
+            }
+            throw new ApplicationValidationExcetion("In Update Have Some Problem");
         }
 
         public async Task<bool> IsCodeExists(string code)
