9b4616b9f9480d48a7c89082cca153d4ed3243f5
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-29 00:36:06 +0600

    Add Some Code

--- University-Api-Project/DLL/DataContext/ApplicationDbContext.cs
+++ University-Api-Project/DLL/DataContext/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DLL.Model.Interfaces;
+using GSF;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using University_Api.Model;
 
 namespace DLL.DataContext
 {
     public class ApplicationDbContext : DbContext
     {
+        
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
@@ -16,5 +24,64 @@ namespace DLL.DataContext
         public DbSet<Student> Students { get; set; }
 
 
+        private const string IsDeletedProperty = "IsDeleted";
+
+        private static readonly MethodInfo _propertyMethod = typeof(EF)
+            .GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(bool));
+        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        {
+            var parm = Expression.Parameter(type, "it");
+            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(IsDeletedProperty));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, parm);
+            return lambda;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(ISoftDeletable).IsAssignableFrom(entity.ClrType) == true)
+                {
+                    entity.AddProperty(IsDeletedProperty, typeof(bool));
+                    modelBuilder.Entity(entity.ClrType).HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+
+                }
+            }
+         
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSavingData();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        private void OnBeforeSavingData()
+        {
+            var entries = ChangeTracker.Entries()
+              .Where(e => e.State != EntityState.Detached && e.State != EntityState.Unchanged);
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is ITrackable trackable)
+                {
+                    switch (entry.State)
+                    {
+
+                  
+                        case EntityState.Deleted:
+                            entry.Property(IsDeletedProperty).CurrentValue = true;
+                            entry.State = EntityState.Modified;
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
