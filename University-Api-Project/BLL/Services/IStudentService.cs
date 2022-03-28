e01c55784808cf8ae7334b137a8a20a0ad6dd3f3
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-28 09:11:32 +0600

    Add BLL Project

---
+++ University-Api-Project/BLL/Services/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Services
+{
+    public interface IStudentService
+    {
+    }
+
+    public class StudentService : IStudentService
+    {
+
+    }
+}
