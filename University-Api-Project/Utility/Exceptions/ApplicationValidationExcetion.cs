02e95697841aa18fb94165daae3d22ac208b0dbb
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-28 19:41:30 +0600

    Add Utility

---
+++ University-Api-Project/Utility/Exceptions/ApplicationValidationExcetion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Exceptions
+{
+    public class ApplicationValidationExcetion : Exception
+    {
+        public ApplicationValidationExcetion(string massage): base (massage)
+        {
+                
+        }
+    }
+}
---
+++ University-Api-Project/Utility/Model/ApiErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Model
+{
+    public class ApiErrorResponse
+    {
+        public string Message { get; set; } 
+        public string Detailes { get; set; }
+        public bool IssSuccess { get; set; } = false;
+
+        public int StatusCode { get; set; }
+    }
+}
