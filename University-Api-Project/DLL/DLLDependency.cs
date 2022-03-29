209670d91ec5bbd3c1eef2b35ad74e869bcd808c
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-29 17:05:56 +0600

    Add All Setup

--- University-Api-Project/DLL/DLLDependency.cs
+++ University-Api-Project/DLL/DLLDependency.cs
@@ -17,6 +17,7 @@ namespace DLL
             //Repository Depandency
             services.AddTransient<IDepartmentRepository, DepartmentRepository>();
             services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<ICourseRepository, CourseRepository>();
 
 
 
---
+++ University-Api-Project/DLL/Repositories/ICourseRepository.cs
@@ -0,0 +1,20 @@
+﻿using DLL.DataContext;
+using DLL.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using University_Api.Model;
+
+namespace DLL.Repositories
+{
+    public interface ICourseRepository : IRepositoryBase<Course>
+    {
+      
+    }
+    public class CourseRepository : RepositoryBase<Course>, ICourseRepository
+    {
+        public CourseRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
