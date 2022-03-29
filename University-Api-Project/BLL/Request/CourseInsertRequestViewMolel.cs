6fb0d080e90471324b543fe482939387827c2989
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-29 17:04:12 +0600

    Add New Requst

--- University-Api-Project/BLL/Request/CourseInsertRequestViewMolel.cs
+++ University-Api-Project/BLL/Request/CourseInsertRequestViewMolel.cs
@@ -7,23 +7,27 @@ using System.Threading.Tasks;
 
 namespace BLL.Request
 {
-    public class DepartmentInsertRequestViewMolel
+    public class CourseInsertRequestViewMolel
     {
         public string Name { get; set; }
         public string Code { get; set; }
+        public decimal Credit { get; set; }
     }
-    public class DepartmentInsertRequestViewMolelValidator : AbstractValidator<DepartmentInsertRequestViewMolel>
+    public class CourseInsertRequestViewMolelValidator : AbstractValidator<CourseInsertRequestViewMolel>
     {
         private readonly IServiceProvider _serviceProvider;
 
-        public DepartmentInsertRequestViewMolelValidator(IServiceProvider serviceProvider)
+        public CourseInsertRequestViewMolelValidator(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
             RuleFor(x => x.Name).NotNull().NotEmpty()
                 .MinimumLength(4).MaximumLength(25).MustAsync(NameExists).WithMessage("Name Ecists In Our System");
 
             RuleFor(x => x.Code).NotNull().NotEmpty()
-                .MinimumLength(3).MaximumLength(10).MustAsync(CodeExists).WithMessage("Code Ecists In Our System"); 
+                .MinimumLength(3).MaximumLength(10).MustAsync(CodeExists).WithMessage("Code Ecists In Our System");
+
+            RuleFor(x => x.Credit).NotNull().NotEmpty();
+              
         }
 
         private async Task<bool> CodeExists(string code, CancellationToken arg2)
@@ -32,7 +36,7 @@ namespace BLL.Request
             {
                 return true;
             }
-            var requiredService = _serviceProvider.GetRequiredService<IDepartmentService>();
+            var requiredService = _serviceProvider.GetRequiredService<ICourseService>();
             return await requiredService.IsCodeExists(code);
         }
 
@@ -42,7 +46,7 @@ namespace BLL.Request
             {
                 return true;
             }
-            var requiredService = _serviceProvider.GetRequiredService<IDepartmentService>();
+            var requiredService = _serviceProvider.GetRequiredService<ICourseService>();
             return await requiredService.IsCodeExists(name);
         }
     }
---
+++ University-Api-Project/BLL/Request/StudentInsertRequestViewMolde.cs
@@ -0,0 +1,56 @@
+﻿using BLL.Services;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BLL.Request
+{
+    public class StudentInsertRequestViewMolde
+    {
+        public string Name { get; set; }    
+        public string Email { get; set; }
+        public int DepartmentId { get; set; }
+    }
+
+
+    public class StudentInsertRequestViewMoldeValidator : AbstractValidator<StudentInsertRequestViewMolde>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public StudentInsertRequestViewMoldeValidator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            RuleFor(x => x.Name).NotNull().NotEmpty()
+                .MinimumLength(4).MaximumLength(50);
+
+            RuleFor(x => x.Email).NotNull().NotEmpty().EmailAddress()
+                .MustAsync(EmailExists).WithMessage("Email Exists In Our System");
+
+            RuleFor(x => x.DepartmentId).GreaterThan(0)
+              .MustAsync(DepartmentExists).WithMessage("Department Not Exists In Our System");
+        }
+
+   
+        private async Task<bool> EmailExists(string email, CancellationToken arg2)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return true;
+            }
+            var requiredService = _serviceProvider.GetRequiredService<IStudentService>();
+            return await requiredService.EmailExists(email);
+        }
+
+        private async Task<bool> DepartmentExists(int id, CancellationToken arg2)
+        {
+            if(id == 0)
+            {
+                return true;
+            }
+            var requiredService = _serviceProvider.GetRequiredService<IDepartmentService>();
+            return ! await requiredService.IsIdExists(id);
+        }
+    }
+}
