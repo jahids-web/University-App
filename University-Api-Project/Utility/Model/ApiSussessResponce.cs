3a8ff723a4d20f192b80887216bc92cd2e63e1a0
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-30 16:25:26 +0600

    Add New ApiSussessResponce

---
+++ University-Api-Project/Utility/Model/ApiSussessResponce.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Model
+{
+    public class ApiSussessResponce
+    {
+        public int StatuseCode { get; set; }
+        public string Message { get; set; }
+    }
+}
