0ac88b5eb564dcfa3fda9885ac80f991bd4a97b2
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-30 16:24:24 +0600

    Debug

---
+++ University-Api-Project/DLL/Repositories/ICourseStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using DLL.DataContext;
+using DLL.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using University_Api.Model;
+
+namespace DLL.Repositories
+{
+    public interface ICourseStudentRepository : IRepositoryBase<CourseStudent>
+    {
+      
+    }
+    public class CourseStudentRepository : RepositoryBase<CourseStudent>, ICourseStudentRepository
+    {
+        public CourseStudentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
