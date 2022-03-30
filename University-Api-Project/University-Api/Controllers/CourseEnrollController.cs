a076ad4d2b1a8a9f6ea8e4f241bb2c72605d0e79
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-30 16:24:53 +0600

    Debug

---
+++ University-Api-Project/University-Api/Controllers/CourseEnrollController.cs
@@ -0,0 +1,23 @@
+﻿using BLL.Request;
+using BLL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace University_Api.Controllers
+{
+    public class CourseEnrollController : MainApiController
+    {
+        private readonly ICourseStudentService _courseStudentService;
+
+        public CourseEnrollController(ICourseStudentService courseStudentService)
+        {
+            _courseStudentService = courseStudentService; 
+        }
+        [HttpPost]
+        public async Task<IActionResult> Insert(CourseAssignInsertViewModel request)
+        {
+            return Ok(await _courseStudentService.InsertAsyce(request));
+        }
+    }
+}
